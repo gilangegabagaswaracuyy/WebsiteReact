45725b6dcc8ec2feefb4c3a2ccc764a08de7ef70
gilangega <gilangegabagaswara@gmail.com>
2025-02-05 12:45:52 +0700

    Fix import path issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DetailPortfolio from "./pages/DetailPortfolio";
 import PageNotFound from "./pages/PageNotFound";
